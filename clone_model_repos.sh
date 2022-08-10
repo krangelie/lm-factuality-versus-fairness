436cb96515bda0912e58bc2af5a01616f09fabe5
Angelie Kraft <angelie.kraft@tutanota.com>
2022-08-10 10:48:05 +0200

    update download script

--- clone_model_repos.sh
+++ clone_model_repos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-git clone git@github.com:allenai/kb.git ./models/knowbert
+
+git clone git@github.com:allenai/kb.git ./models/knowbert/source
 wget https://allennlp.s3-us-west-2.amazonaws.com/knowbert/models/knowbert_wordnet_model.tar.gz -O ./models/knowbert/knowbert_wordnet_model.tar.gz
