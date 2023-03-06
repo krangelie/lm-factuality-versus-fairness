d85686dbcd601a98a5ab7b4bcf000a948d528c7f
krangelie <kraftangelie@gmail.com>
2023-03-06 14:12:37 +0100

    add bold test computation to main experiment script

--- run_experiment.py
+++ run_experiment.py
@@ -5,7 +5,7 @@ from omegaconf import DictConfig
 from stereoset.experiment import run_experiment
 from stereoset.stereoset_evaluation import run_evaluation
 from bold.generate import generate_bold_sentences
-
+from bold.run_tests import run_polarity_tests
 
 @hydra.main(config_path="conf", config_name="config")
 def main(cfg: DictConfig):
@@ -17,7 +17,10 @@ def main(cfg: DictConfig):
         else:
             sys.exit(f"Undefined mode: {cfg.benchmark.mode}")
     elif cfg.benchmark.name == "bold":
-        generate_bold_sentences(cfg)
+        if cfg.benchmark.generate_bold_sentences:
+            generate_bold_sentences(cfg)
+        if cfg.benchmark.evaluate_bold_sentences:
+            run_polarity_tests(cfg)
     else:
         sys.exit(f"Undefined benchmark: {cfg.benchmark.name}")
 
