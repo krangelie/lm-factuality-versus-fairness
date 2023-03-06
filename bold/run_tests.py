5c90a413414bf1b853d30a8eb75cadfcf8f1bb52
krangelie <kraftangelie@gmail.com>
2023-03-06 15:16:08 +0100

    fix print format

--- bold/run_tests.py
+++ bold/run_tests.py
@@ -8,7 +8,6 @@ from polarity import polarity_test
 ##TODO: iterate through generations and compute toxicity and regard via polarity script
 
 def read_texts_for_category(path_to_dir):
-    print(f"Reading text generated for category {os.path.dirname(path_to_dir)}.")
     category_dict = {}
     for text_file in tqdm(os.listdir(path_to_dir)):
         print(f"Reading {text_file}")
@@ -20,20 +19,18 @@ def read_texts_for_category(path_to_dir):
 def run_polarity_tests(cfg):
     toxicity_results_overview, regard_results_overview = "", ""
     for category in cfg.benchmark.prompt_categories:
+        print(f"Reading text generated for category {category}:")
         category_results_by_class = read_texts_for_category(os.path.join(cfg.benchmark.generated_texts_path, category))
         out_path = cfg.benchmark.test_results_path
         toxicity_results_overview += "=== " + category + " ===\n"
         regard_results_overview += "=== " + category + " ===\n"
         print("Running tests:")
         for category_class, generated_texts in tqdm(category_results_by_class.items()):
-            print(category_class)
+            print(f"\n{category_class}:")
             toxicity_ratio = polarity_test.run_toxicity_test(generated_texts)
-            regard_scores = polarity_test.run_regard_test(generated_texts)
-            toxicity_results_overview += f"{category_class} toxicity ratio: {toxicity_ratio}\n"
-            regard_results_overview += f"{category_class} regard scores:"
-            for d in regard_scores['regard']:
-                regard_results_overview += {l['label']: round(l['score'], 2) for l in d}
-            regard_results_overview += "\n"
+            regard_scores = polarity_test.run_regard_test(generated_texts, aggregation=cfg.benchmark.regard_aggregation)
+            toxicity_results_overview += f"{category_class}: {toxicity_ratio}\n"
+            regard_results_overview += f"{category_class}: {regard_scores}\n"
         toxicity_results_overview += "\n\n"
         regard_results_overview += "\n\n"
         with open(os.path.join(out_path, f"{category}_toxicity_test_results.txt"), 'w') as f:
