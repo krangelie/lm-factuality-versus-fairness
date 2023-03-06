c55570cde5407e89f89695287f5ec2fa04d924b6
krangelie <kraftangelie@gmail.com>
2023-03-06 15:15:58 +0100

    adding aggregation method for regard testing

--- polarity/polarity_test.py
+++ polarity/polarity_test.py
@@ -6,16 +6,24 @@ import evaluate
 def run_toxicity_test(generated_texts):
     toxicity = evaluate.load("toxicity")
     toxicity_ratio = toxicity.compute(predictions=generated_texts, aggregation="ratio")
-    print("Toxicity ratio:", toxicity_ratio)
+    print(toxicity_ratio)
     return toxicity_ratio
 
 
 def run_regard_test(generated_texts_group_1, generated_texts_group_2=None, aggregation=None):
+    # regard : the regard scores of each string in the input list (if no aggregation is specified)
+    # average_regard: the average regard for each category (negative, positive, neutral, other) (if aggregation = average)
+    # max_regard: the maximum regard across all input strings (if aggregation = maximum)
     if generated_texts_group_2 is None:
         regard = evaluate.load("regard")
-        results = regard.compute(data=generated_texts_group_1)
-        for d in results['regard']:
-            print("Regard scores:", {l['label']: round(l['score'], 2) for l in d})
+        results = regard.compute(data=generated_texts_group_1, aggregation=aggregation)
+        if aggregation is None or aggregation == "regard":
+            for d in results["regard"]:
+                print("Regard scores:", {l['label']: round(l['score'], 2) for l in d})
+        else:
+            print("Aggregation:", aggregation)
+            print("Regard scores:", results)
+
     else:
         # aggregation = None: Compute regard difference; aggregation = average: avg. per group
         regard = evaluate.load("regard", "compare")
@@ -24,6 +32,6 @@ def run_regard_test(generated_texts_group_1, generated_texts_group_2=None, aggre
                                  aggregation=aggregation)
         print("Regard differences:", {k: round(v, 2) for k, v in results['regard_difference'].items()})
 
-        return results
+    return results
 
 
